fdfc32cf427ea1d991b6294522e51b62af68eafe
Steven Chou <stevennote1130@gmail.com>
2017-05-29 16:28:31 +0800

    Created project

---
+++ TaipeiParks/ParkTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ParkTableViewCell.swift
+//  TaipeiParks
+//
+//  Created by steven.chou on 2017/5/29.
+//  Copyright © 2017年 stevenchou. All rights reserved.
+//
+
+import UIKit
+
+class ParkTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ TaipeiParks/ParksTableViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ParksTableViewController.swift
+//  TaipeiParks
+//
+//  Created by steven.chou on 2017/5/29.
+//  Copyright © 2017年 stevenchou. All rights reserved.
+//
+
+import UIKit
+
+class ParksTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+}
